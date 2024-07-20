6ade9045320e7f4b9eed6884af21ace09b46da2e
Rohit Mogal <Rohit.Mogal@winjit.com>
2024-07-20 22:30:44 +0530

    create user api for example

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -2,7 +2,14 @@ const userServices = require("../services/userService");
 
 const createUser = async (req, res) => {
   try {
-    userServices.createUser();
+    console.log(req.body);
+    const { fullName, email, password } = req.body;
+    const result = await userServices.createUser(fullName, email, password);
+    if (result) {
+      res.status(200).json({ message: "User created sucessfully" });
+    } else {
+      res.status(400).json({ error: "User creation failed" });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- Backend/src/services/userService.js
+++ Backend/src/services/userService.js
@@ -1,2 +1,17 @@
-const createUser = async () => {};
+const query1 = require("../config/db_config");
+
+const createUser = async (fullName, email, password) => {
+  try {
+    const query = `INSERT INTO Users (guid, FullName, email, password) VALUES (UUID(), ?, ?, ?);`;
+    const result = await query1.executeQuery(query, [
+      fullName,
+      email,
+      password,
+    ]);
+    return result;
+  } catch (err) {
+    throw new Error("Error creating user: " + err.message);
+  }
+};
+
 module.exports = { createUser };
